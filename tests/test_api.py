047662340c4d4b753fb225f407b8fb432b204c1a
Sai Venkata Dheeraj Talasila <78401163+dheerajtsv@users.noreply.github.com>
2026-06-24 21:32:39 +0530

    added path

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,6 +1,8 @@
 from fastapi.testclient import TestClient
 from api import app
+import os
 
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 client = TestClient(app)
 
 def test_invalid_login():
